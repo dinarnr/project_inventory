cff79ea00e73bcbcfd0d5ba36dd17d2dbe048788
Hadafi337 <85341971+Hadafi337@users.noreply.github.com>
2021-09-23 16:02:57 +0700

    Add files via upload

---
+++ database/migrations/2021_09_22_121459_profil_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProfilTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profil', function (Blueprint $table) {
+            $table->id();
+            $table->string('telp',50);
+            $table->string('email',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profil');
+    }
+}
